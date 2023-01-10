0c4924ee67b83dc3d8c0cfa9092d157a142297ef
leobozzi <lbozzi@gmail.com>
2023-01-10 13:54:35 -0300

    update initial commit

--- moreno_default/__manifest__.py
+++ moreno_default/__manifest__.py
@@ -268,6 +268,7 @@
         # 'https://github.com/OCA/account-closing oca-account-closing',
         'https://github.com/OCA/account-financial-reporting oca-account-financial-reporting',  # noqa
         'https://github.com/OCA/account-financial-tools oca-account-financial-tools',
+        'https://github.com/OCA/account-invoicing oca-account-invoicing',
         'https://github.com/OCA/account-payment oca-account-payment',
         # 'https://github.com/OCA/apps-store oca-apps-store',
         'https://github.com/OCA/bank-payment oca-bank-payment',
